d8a81bd68eae40f46a3aeb1e6060bb4cba871c0b
Go Hayakawa <go.hayakawa@gmail.com>
2020-06-29 23:16:25 +0900

    tmp

--- build.sh
+++ build.sh
@@ -8,10 +8,7 @@ ALIS_APP_DOMAIN=`aws ssm get-parameter --name ${ALIS_APP_ID}ssmAlisAppDomain --q
 export ALIS_APP_DOMAIN=${ALIS_APP_DOMAIN}
 BASE_URL=https://${ALIS_APP_DOMAIN}
 export BASE_URL=${BASE_URL}
-COGNITO_USER_POOL_ID=`aws ssm get-parameter --name ${ALIS_APP_ID}ssmCognitoUserPoolId --query "Parameter.Value" --output text`
-export COGNITO_USER_POOL_ID=${COGNITO_USER_POOL_ID}
-COGNITO_APP_CLIENT_ID=`aws ssm get-parameter --name ${ALIS_APP_ID}ssmCognitoUserPoolAppId --query "Parameter.Value" --output text`
-export COGNITO_APP_CLIENT_ID=${COGNITO_APP_CLIENT_ID}
+
 DIST_S3_BUCKET_NAME=`aws ssm get-parameter --name ${ALIS_APP_ID}ssmDistS3BucketName --query "Parameter.Value" --output text`
 export DIST_S3_BUCKET_NAME=${DIST_S3_BUCKET_NAME}
 IFRAMELY_API_KEY=`aws ssm get-parameter --name ${ALIS_APP_ID}ssmIframelyApiKey --query "Parameter.Value" --output text`
@@ -30,19 +27,25 @@ PRIVATE_CHAIN_ALIS_TOKEN_ADDRESS=`aws ssm get-parameter --name ${ALIS_APP_ID}ssm
 export PRIVATE_CHAIN_ALIS_TOKEN_ADDRESS=${PRIVATE_CHAIN_ALIS_TOKEN_ADDRESS}
 PRIVATE_CHAIN_BRIDGE_ADDRESS=`aws ssm get-parameter --name ${ALIS_APP_ID}ssmPrivateChainBridgeAddress --query "Parameter.Value" --output text`
 export PRIVATE_CHAIN_BRIDGE_ADDRESS=${PRIVATE_CHAIN_BRIDGE_ADDRESS}
-BURN_ADDRESS=`aws ssm get-parameter --name ${ALIS_APP_ID}ssmBurnAddress --query "Parameter.Value" --output text`
-export BURN_ADDRESS=${BURN_ADDRESS}
 DAILY_LIMIT_TOKEN_SEND_VALUE=`aws ssm get-parameter --name ${ALIS_APP_ID}ssmDailyLimitTokenSendValue --query "Parameter.Value" --output text`
 export DAILY_LIMIT_TOKEN_SEND_VALUE=${DAILY_LIMIT_TOKEN_SEND_VALUE}
-SENTRY_DSN=`aws ssm get-parameter --name ${ALIS_APP_ID}ssmSentryDSN --query "Parameter.Value" --output text`
-export SENTRY_DSN=${SENTRY_DSN}
 SENTRY_RELEASE=`git rev-parse HEAD`
 export SENTRY_RELEASE=${SENTRY_RELEASE}
-COGNITO_IDENTITY_POOL_ID=`aws ssm get-parameter --name ${ALIS_APP_ID}ssmCognitoIdentityPoolId --query "Parameter.Value" --output text`
+
+# Use staging
+COGNITO_USER_POOL_ID=`aws ssm get-parameter --name stagingssmCognitoUserPoolId --query "Parameter.Value" --output text`
+export COGNITO_USER_POOL_ID=${COGNITO_USER_POOL_ID}
+COGNITO_APP_CLIENT_ID=`aws ssm get-parameter --name stagingssmCognitoUserPoolAppId --query "Parameter.Value" --output text`
+export COGNITO_APP_CLIENT_ID=${COGNITO_APP_CLIENT_ID}
+COGNITO_IDENTITY_POOL_ID=`aws ssm get-parameter --name stagingssmCognitoIdentityPoolId --query "Parameter.Value" --output text`
 export COGNITO_IDENTITY_POOL_ID=${COGNITO_IDENTITY_POOL_ID}
-ALL_TOKEN_HISTORY_CSV_DWONLOAD_S3_BUCKET=`aws ssm get-parameter --name ${ALIS_APP_ID}ssmAllTokenHistoryCsvDownloadS3Bucket --query "Parameter.Value" --output text`
+ALL_TOKEN_HISTORY_CSV_DWONLOAD_S3_BUCKET=`aws ssm get-parameter --name stagingssmAllTokenHistoryCsvDownloadS3Bucket --query "Parameter.Value" --output text`
 export ALL_TOKEN_HISTORY_CSV_DWONLOAD_S3_BUCKET=${ALL_TOKEN_HISTORY_CSV_DWONLOAD_S3_BUCKET}
-export WEB3_SERVICE_BASE_URL=`aws ssm get-parameter --name ${ALIS_APP_ID}ssmWeb3ServiceBaseUrl --query "Parameter.Value" --output text`
+export WEB3_SERVICE_BASE_URL=`aws ssm get-parameter --name stagingssmWeb3ServiceBaseUrl --query "Parameter.Value" --output text`
+BURN_ADDRESS=`aws ssm get-parameter --name stagingssmBurnAddress --query "Parameter.Value" --output text`
+export BURN_ADDRESS=${BURN_ADDRESS}
+SENTRY_DSN=`aws ssm get-parameter --name stagingssmSentryDSN --query "Parameter.Value" --output text`
+export SENTRY_DSN=${SENTRY_DSN}
 
 if [ $1 = 'ci' ];then
   yarn nuxt build --config-file nuxt.config.ci.js
--- nuxt.config.ci.js
+++ nuxt.config.ci.js
@@ -1,14 +1,14 @@
 // CI環境でのみ利用する設定
 const nuxtConfig = require('./nuxt.config.js')
 
-// Sentryへのreleaseとsourcemapの反映を行う
-nuxtConfig.sentry = {
-  dsn: `${process.env.SENTRY_DSN}`,
-  publishRelease: true,
-  config: {
-    release: `${process.env.SENTRY_RELEASE}`,
-    ignoreErrors: [/^view-createPositionAt-offset-required.*/]
-  }
-}
+// // Sentryへのreleaseとsourcemapの反映を行う
+// nuxtConfig.sentry = {
+//   dsn: `${process.env.SENTRY_DSN}`,
+//   publishRelease: true,
+//   config: {
+//     release: `${process.env.SENTRY_RELEASE}`,
+//     ignoreErrors: [/^view-createPositionAt-offset-required.*/]
+//   }
+// }
 
 module.exports = nuxtConfig
